6fb691c5774a7eb13a65597d638814d061d0c965
TanishaRoy04 <rtanisha065@gmail.com>
2026-02-25 23:21:25 +0530

    Add files via upload

---
+++ day25.c
@@ -0,0 +1,64 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+// Define the structure for a node
+struct Node {
+    int data;
+    struct Node* next;
+};
+
+// Function to create a new node
+struct Node* createNode(int value) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = value;
+    newNode->next = NULL;
+    return newNode;
+}
+
+// Function to count occurrences of key
+int countOccurrences(struct Node* head, int key) {
+    int count = 0;
+    struct Node* temp = head;
+
+    while (temp != NULL) {
+        if (temp->data == key) {
+            count++;
+        }
+        temp = temp->next;
+    }
+
+    return count;
+}
+
+int main() {
+    int n, value, key;
+
+    struct Node *head = NULL, *temp = NULL, *newNode = NULL;
+
+    // Input number of nodes
+    scanf("%d", &n);
+
+    // Create linked list
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &value);
+        newNode = createNode(value);
+
+        if (head == NULL) {
+            head = newNode;
+        } else {
+            temp->next = newNode;
+        }
+        temp = newNode;
+    }
+
+    // Input key
+    scanf("%d", &key);
+
+    // Count occurrences
+    int result = countOccurrences(head, key);
+
+    // Output result
+    printf("%d\n", result);
+
+    return 0;
+}
